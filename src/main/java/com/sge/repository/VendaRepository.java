dbb5853d91d4579eea84757af1cf5846bc47e529
Andrey <andreybr2002@hotmail.com>
2022-06-27 22:28:28 -0300

    Implementado Venda Repository

---
+++ src/main/java/com/sge/repository/VendaRepository.java
@@ -0,0 +1,10 @@
+package com.sge.repository;
+
+import com.sge.model.Venda;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendaRepository extends JpaRepository<Venda, Long> {
+    Page<Venda> findAll(Pageable page);
+}
