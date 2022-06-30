4786bd7a612977aac241ff47286aa7baa9994280
Andrey <andreybr2002@hotmail.com>
2022-06-30 20:50:58 -0300

    Implementado EnderecoController para o consumo do Brasil API

---
+++ src/main/java/com/sge/controller/EnderecoController.java
@@ -0,0 +1,27 @@
+package com.sge.controller;
+
+import com.sge.exceptions.ResourceNotFoundException;
+import com.sge.model.model.Endereco;
+import com.sge.service.EnderecoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class EnderecoController {
+    @Autowired
+    private EnderecoService enderecoService;
+
+    @GetMapping(value = "/endereco/{cep}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Endereco> findEnderecoByCep(@PathVariable Long cep) throws ResourceNotFoundException {
+        Endereco endereco = enderecoService.findByCep(cep);
+        return ResponseEntity.ok(endereco);
+    }
+}
