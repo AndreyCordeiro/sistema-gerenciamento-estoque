0679ee27bf7f27849e5c058449f636bad42ffbfe
Andrey <andreybr2002@hotmail.com>
2022-07-01 22:22:50 -0300

    Implementando Repository do ItensVenda

---
+++ src/main/java/com/sge/repository/ItensVendaRepository.java
@@ -0,0 +1,7 @@
+package com.sge.repository;
+
+import com.sge.model.entity.ItensVenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItensVendaRepository extends JpaRepository<ItensVenda, Long> {
+}
