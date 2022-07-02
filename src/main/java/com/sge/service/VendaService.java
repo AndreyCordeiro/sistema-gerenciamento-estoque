2f92a812ae6e1fdeebcbd9faa24ac4616bfdf825
Andrey <andreybr2002@hotmail.com>
2022-07-01 22:22:05 -0300

    Corrigindo erro de StackOverflow nos itens da venda

--- src/main/java/com/sge/service/VendaService.java
+++ src/main/java/com/sge/service/VendaService.java
@@ -3,8 +3,11 @@ package com.sge.service;
 import com.sge.exceptions.BadResourceException;
 import com.sge.exceptions.ResourceAlreadyExistsException;
 import com.sge.exceptions.ResourceNotFoundException;
+import com.sge.model.entity.ItensVenda;
 import com.sge.model.entity.Venda;
 import com.sge.repository.VendaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,7 +16,9 @@ import org.springframework.stereotype.Service;
 import java.lang.module.ResolutionException;
 
 @Service
-public class VendaService {
+public class VendaService extends ItensVendaService {
+    public static final Logger logger = LoggerFactory.getLogger(VendaService.class);
+
     @Autowired
     private VendaRepository vendaRepository;
 
@@ -37,12 +42,20 @@ public class VendaService {
 
     public Venda salvarVenda(Venda venda) throws BadResourceException, ResourceAlreadyExistsException {
         if (venda != null) {
-            return vendaRepository.save(venda);
+            Venda vendaSalva = vendaRepository.save(venda);
+            logger.info("Venda " + venda.getId() + " salva com sucesso!");
+
+            for (ItensVenda itens : vendaSalva.getItensVenda()) {
+                itens.setVenda(vendaSalva);
+                salvarItensVenda(itens);
+            }
+            logger.info("O(s) item(ns) da venda " + venda.getId() + " foi(ram) salvo(s) com sucesso!");
         } else {
             BadResourceException badResourceException = new BadResourceException("Erro ao realizar venda");
             badResourceException.addErrorMessage("Venda está vazia ou é nula");
             throw badResourceException;
         }
+        return venda;
     }
 
     public void deleteById(Long id) throws ResourceNotFoundException {
