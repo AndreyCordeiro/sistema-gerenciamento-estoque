e074acc51805ae4a8589d6ae4db9121d38bc8013
magalhaesmth <matheusurbano2002@hotmail.com>
2022-07-04 18:00:44 -0300

    Implementando CargoFuncionarioRepostory

---
+++ src/main/java/com/sge/repository/CargoFuncionarioRepository.java
@@ -0,0 +1,7 @@
+package com.sge.repository;
+
+import com.sge.model.entity.CargoFuncionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CargoFuncionarioRepository extends JpaRepository<CargoFuncionario, Long> {
+}
