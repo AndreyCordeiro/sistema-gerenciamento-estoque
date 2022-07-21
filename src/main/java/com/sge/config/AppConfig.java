e180ada99bf507b4d37f5aa39f4d498493b4d243
Andrey <andreybr2002@hotmail.com>
2022-07-21 20:48:33 -0300

    Implementando criptografia de senha

---
+++ src/main/java/com/sge/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.sge.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+}
