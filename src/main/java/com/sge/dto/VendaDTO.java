58c34496eb23ca939f4746bcfa23faef24f8f689
Matheus Magalhães Urbano <89428438+magalhaesmth@users.noreply.github.com>
2022-07-25 22:24:34 -0300

    FindById com DTO

--- src/main/java/com/sge/dto/VendaDTO.java
+++ src/main/java/com/sge/dto/VendaDTO.java
@@ -1,32 +1,74 @@
-package com.sge.dto;
+package com.sge.controller;
 
-import com.sge.model.entity.Cliente;
-import com.sge.model.entity.Funcionario;
-import com.sge.model.entity.ItensVenda;
+import com.sge.dto.ClienteDTO;
+import com.sge.dto.VendaDTO;
+import com.sge.exceptions.BadResourceException;
+import com.sge.exceptions.ResourceAlreadyExistsException;
+import com.sge.exceptions.ResourceNotFoundException;
 import com.sge.model.entity.Venda;
-import lombok.Data;
-
-import java.util.Date;
-import java.util.List;
-
-import org.springframework.beans.BeanUtils;
+import com.sge.service.VendaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-@Data
-public class VendaDTO {
-	
-	private Date dataVenda = new Date();
-	private Funcionario funcionario;
-	private Cliente cliente;
-	private List<ItensVenda> itensVenda;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-    public VendaDTO convert(Venda venda) {
-        VendaDTO vendaDTO = new VendaDTO();
-        BeanUtils.copyProperties(venda, vendaDTO);
-        return vendaDTO;
+@RestController
+@RequestMapping("/api")
+public class VendaController {
+    public static final Logger logger = LoggerFactory.getLogger(VendaController.class);
+
+    @Autowired
+    private VendaService vendaService;
+
+    @GetMapping(value = "/venda", consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Page<VendaDTO>> findAll(@RequestBody(required = false) String nome, Pageable pageable) {
+        if (StringUtils.isEmpty(nome)) {
+        	return ResponseEntity.ok(new VendaDTO().convertVenda(vendaService.findAll(pageable)));
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
     }
 
-    public Page<VendaDTO> convertVenda(Page<Venda> pageVenda) {
-        return pageVenda.map(this::convert);
+    @GetMapping(value = "/venda/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<VendaDTO> findVendaById(@PathVariable Long id) {
+        Venda venda = vendaService.findById(id);
+        VendaDTO vendaDTO = new VendaDTO().convert(venda);
+        return ResponseEntity.ok(vendaDTO);
+    }
+    
+    @PostMapping(value = "/venda")
+    public ResponseEntity<Venda> realizarVenda(@RequestBody Venda venda)
+            throws URISyntaxException {
+        try {
+            Venda vendaRealizada = vendaService.salvarVenda(venda);
+            return ResponseEntity.created(new URI("/api/venda" + vendaRealizada.getId())).body(venda);
+        } catch (ResourceAlreadyExistsException | BadResourceException e) {
+            logger.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @DeleteMapping(path = "/venda/{id}")
+    public ResponseEntity<Void> deleteVendaById(@PathVariable Long id) {
+        try {
+            vendaService.deleteById(id);
+            logger.info("A venda " + id + " foi deletada");
+            return ResponseEntity.ok().build();
+        } catch (ResourceNotFoundException ex) {
+            logger.error(ex.getMessage());
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "A venda " + id + " não foi encontrada");
+        }
     }
 }
