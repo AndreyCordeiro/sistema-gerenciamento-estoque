1c396fba853edfa94a61ead90eb6223d262bb69f
Matheus Magalhães Urbano <89428438+magalhaesmth@users.noreply.github.com>
2022-07-25 22:06:25 -0300

    Atualizando VendaDTO

--- src/main/java/com/sge/dto/VendaDTO.java
+++ src/main/java/com/sge/dto/VendaDTO.java
@@ -1,17 +1,24 @@
 package com.sge.dto;
 
+import com.sge.model.entity.Cliente;
+import com.sge.model.entity.Funcionario;
+import com.sge.model.entity.ItensVenda;
 import com.sge.model.entity.Venda;
 import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 
 @Data
 public class VendaDTO {
-    private String nome;
-    private String usuario;
-    private String cpf;
-    private String telefone;
-    private String email;
+	
+	private Date dataVenda = new Date();
+	private Funcionario funcionario;
+	private Cliente cliente;
+	private List<ItensVenda> itensVenda;
 
     public VendaDTO convert(Venda venda) {
         VendaDTO vendaDTO = new VendaDTO();
